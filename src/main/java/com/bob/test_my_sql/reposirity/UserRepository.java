1f7d11b29c98395df0315e72742fcda8cfc4882b
ycxing.seu.edu.cn <ycxing.seu.edu.cn>
2018-03-24 19:05:35 +0800

    add: 1.socket server for machine 2.add user entity 3 add machine entity

--- src/main/java/com/bob/test_my_sql/reposirity/UserRepository.java
+++ src/main/java/com/bob/test_my_sql/reposirity/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User,Integer> {
     public List<User> findByUserName(String userName);
     public List<User> deleteByUserName(String userName);
+    
 }
 
